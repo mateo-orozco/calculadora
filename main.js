6e6ccb4b8c52874131d1f998b74352eee04367d1
mateo.O <oromateo@hotmail.com>
2022-09-29 09:09:23 -0500

    calculadora v2.0(solo un imput)

--- main.js
+++ main.js
@@ -6,23 +6,23 @@ const resta = document.getElementById("min")
 const multi = document.getElementById("multi")
 const division = document.getElementById("divi")
 const igual = document.getElementById("equal")
-
+const borrar = document.getElementById("AC")
 igual.addEventListener("click",()=>{
     switch (operador.innerText) {
         case "+" :
-            display.innerText=(parseInt(valor[0].value)  +  parseInt(valor[1].value))
+            display.innerText=(parseInt(valor[1].value)  +  parseInt(valor[0].value))
             break;
         case "-":
-            display.innerText=(parseInt(valor[0].value)  -  parseInt(valor[1].value))
+            display.innerText=(parseInt(valor[1].value)  -  parseInt(valor[0].value))
             break;
         case "*":
-            display.innerText=(parseInt(valor[0].value)  *  parseInt(valor[1].value))
+            display.innerText=(parseInt(valor[1].value)  *  parseInt(valor[0].value))
             break;
         case "/":
             if(parseInt(valor[1].value)===0){
                 alert("NO se puede divir sobre 0")
             }else{
-                display.innerText= (parseInt(valor[0].value)  /  parseInt(valor[1].value)).toFixed(8)
+                display.innerText= (parseInt(valor[1].value)  /  parseInt(valor[0].value)).toFixed(8)
             }
             break;
         default:
@@ -30,15 +30,28 @@ igual.addEventListener("click",()=>{
             break;
     }
 })
+function limpiar(){
+    valor[1].value = valor[0].value
+    valor[0].value=""
+}
 suma.addEventListener("click",()=>{
     operador.innerText = "+"
+    limpiar();
 })
 resta.addEventListener("click",()=>{
     operador.innerText = "-"
+    limpiar();
 })
 multi.addEventListener("click",()=>{
     operador.innerText = "*"
+    limpiar();
 })
 division.addEventListener("click",()=>{
     operador.innerText = "/"
+    limpiar();
+})
+borrar.addEventListener("click",()=>{
+    valor[1].value = "";
+    valor[0].value ="";
+    display.innerText = ""
 })
